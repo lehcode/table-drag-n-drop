645f32b35c58ab51b8151ae8b54dacca701083a1
lehcode <53556648+lehcode@users.noreply.github.com>
2024-07-31 23:35:21 +0300

    hotfix: Relax CORS

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
-  app.enableCors();
+  app.enableCors({origin: "*"});
   const port = process.env.PORT || 3000;
   await app.listen(port);
   Logger.log(
