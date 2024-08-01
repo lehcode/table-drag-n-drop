f9e321f44353139c2822ce1c0c27ebca9552f93d
lehcode <53556648+lehcode@users.noreply.github.com>
2024-08-01 19:21:46 +0300

    chore: Configure NX testing environment

--- front/jest.config.ts
+++ front/jest.config.ts
@@ -2,10 +2,5 @@
 export default {
   displayName: 'front',
   preset: '../jest.preset.js',
-  transform: {
-    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
-    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/react/babel'] }],
-  },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../coverage/front',
 };
